a10871301f6d7acee9731254c3920dd84203f149
Kabyl <kabyl@example.com>
2025-05-06 20:43:21 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/frontend",
   css: {
     postcss: {
       plugins: [
