e0a71be594513e3e81292303b9ffb4616221687d
Kabyl <kabyl@example.com>
2025-05-13 17:54:15 +0500

    Удаление директории frontend и оптимизация структуры проекта

--- frontend/src/features/auth/index.js
+++
@@ -1,3 +0,0 @@
-export { ToggleableEmailForm } from './ui/ToggleableEmailForm';
-export { LoginForm } from './ui/LoginForm';
-export { RegisterForm } from './ui/RegisterForm'; 
--- frontend/src/shared/index.js
+++
@@ -1,9 +0,0 @@
-// Lib
-export * from './context/AuthContext';
-
-// API
-export * from './api/auth';
-
-// UI
-export { default as Button } from './ui/Button';
-export { default as Input } from './ui/Input'; 
