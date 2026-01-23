ed70f7bbc72553a3294f6b2c8df1891825388a56
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2026-01-23 17:27:13 +0500

    fix: understandable message for users

--- src/shared/lib/validation/index.js
+++ src/shared/lib/validation/index.js
@@ -1,5 +1,6 @@
 export { 
   validateUserProfile, 
   useProfileValidation,
-  REQUIRED_PROFILE_FIELDS 
+  REQUIRED_PROFILE_FIELDS_INDIVIDUAL,
+  REQUIRED_PROFILE_FIELDS_LEGAL
 } from './profileValidation';
