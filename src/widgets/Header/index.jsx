eb512db0d63870b75b60d4623f194798c4d88878
Biba <beibarys.shakiratov@gmail.com>
2026-02-24 13:16:50 +0500

    hero section

--- src/widgets/Header/index.jsx
+++ src/widgets/Header/index.jsx
@@ -98,8 +98,8 @@ export const Header = memo(() => {
           clsx(
               "fixed top-0 left-0 right-0 z-50 transition-all duration-300 w-full",
               isScrolled
-                  ? "bg-[#FFF] backdrop-blur-sm shadow-md"
-                  : "bg-[#FFF] backdrop-blur-sm"
+                  ? "bg-[#FFF] backdrop-blur-sm shadow-md translate-y-0"
+                  : "bg-[#FFF] backdrop-blur-sm -translate-y-full"
           )
       , [isScrolled]);
 
